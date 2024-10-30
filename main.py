db461cfb7ae3f5332258d4458db89010f09c4e02
w3schools-test <test@w3schools.com>
2024-10-30 11:04:59 -0500

    fixed models file, added a index and a query method

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
-from sqlalchemy.orm import sessionmaker, declarative_base, relationship
 from models.model import *
 import seaborn as sns
 import matplotlib.pyplot as plt
 import os
 
 app = Flask(__name__)
-app.secret_key = "RunCurrentFile"
+app.secret_key = "stdyfugihjfhgfyu"
+session = SS()
+supply = Supply('drill', 300, 1.59)
+supply_total = supply.calc_total_supply_cost()
+room = Room('hi',149.54, "HardWood", 5,'y','Ceramic',5.43, 20, supply_total)
+session.add(room)
+session.commit()
+session.close()
+@app.route("/")
+def index():
+    return render_template('index.html', All_Rooms = get_all_rooms_being_Remodeled() )
+
+
+
+def get_all_rooms_being_Remodeled():
+    return session.query(Room).join(Supply).filter(Room.is_tiling_needed == 'y').all()
 
 if __name__ == "__main__":
     app.run(debug=True)
