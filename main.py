b48391e54e7269841c506a930b7ddbdda262ce5c
w3schools-test <test@w3schools.com>
2024-10-31 10:15:30 -0500

    removed test code for the front end display

--- main.py
+++ main.py
@@ -8,12 +8,6 @@ import os
 app = Flask(__name__)
 app.secret_key = "stdyfugihjfhgfyu"
 session = SS()
-supply = Supply('drill', 300, 1.59)
-supply_total = supply.calc_total_supply_cost()
-room = Room('hi',149.54, "HardWood", 5,'y','Ceramic',5.43, 20, supply_total)
-session.add(room)
-session.commit()
-session.close()
 @app.route("/")
 def index():
     return render_template('index.html', All_Rooms = get_all_rooms_being_Remodeled() )
@@ -21,7 +15,7 @@ def index():
 
 
 def get_all_rooms_being_Remodeled():
-    return session.query(Room).join(Supply).filter(Room.is_tiling_needed == 'y').all()
+    return session.query(Room).filter(Room.is_tiling_needed == 'y').all()
 
 if __name__ == "__main__":
     app.run(debug=True)
