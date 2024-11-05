0beda1bef59526a92b887dcd392522cfdabb24eb
Juce8888 <keleallison6@gmail.com>
2024-11-04 21:28:39 -0600

    Completed a test to build a room

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models.model import *
 
+
 @pytest.fixture(scope='module')
 def test_session():
     engine = create_engine('sqlite:///:memory:')
@@ -11,6 +12,7 @@ def test_session():
     session = Session()
     yield session
     session.close()
+
 
 def test_room(test_session):
     new_room = Room(
@@ -22,5 +24,19 @@ def test_room(test_session):
         tile_type="glass",
         tile_cost_per_sqft=3.75,
         tiling_area=3.00,
-        total_supply_cost=
+        total_supply_cost=0
     )
+
+    test_session.add(new_room)
+    test_session.commit()
+
+    room = test_session.query(Room).first()
+    assert room.name == "BedRoom"
+    assert room.surface_area == 25.5
+    assert room.flooring_type == "hardwood"
+    assert room.flooring_cost_per_sqft == 1.25
+    assert room.is_tiling_needed == "1"
+    assert room.tile_type == "glass"
+    assert room.tile_cost_per_sqft == 3.75
+    assert room.tiling_area == 3.00
+    assert room.total_supply_cost == 0
