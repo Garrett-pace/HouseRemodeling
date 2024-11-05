4a8d8b519832f1827d8b8c17d50435594f9417aa
Juce8888 <keleallison6@gmail.com>
2024-11-04 22:38:38 -0600

    Added a little more testing and am working on testing the calcs

--- test.py
+++ test.py
@@ -40,3 +40,53 @@ def test_room(test_session):
     assert room.tile_cost_per_sqft == 3.75
     assert room.tiling_area == 3.00
     assert room.total_supply_cost == 0
+
+
+def test_supply(test_session):
+    new_room = Room(
+        name="BedRoom",
+        surface_area=25.50,
+        flooring_type="hardwood",
+        flooring_cost_per_sqft=1.25,
+        is_tiling_needed=True,
+        tile_type="glass",
+        tile_cost_per_sqft=3.75,
+        tiling_area=3.00,
+        total_supply_cost=0
+    )
+
+    test_session.add(new_room)
+    test_session.commit()
+
+    supply = Supply(
+        name="BedSupply",
+        quantity=250,
+        cost_per_item=5.25
+    )
+
+    test_session.add(supply)
+    test_session.commit()
+
+    supply = test_session.query(Supply).first()
+    assert supply.name == "BedSupply"
+    assert supply.quantity == 250
+    assert supply.cost_per_item == 5.25
+
+
+def test_calc_tile_cost(test_session):
+    new_room = Room(
+        name="BedRoom",
+        surface_area=25.50,
+        flooring_type="hardwood",
+        flooring_cost_per_sqft=1.25,
+        is_tiling_needed=True,
+        tile_type="glass",
+        tile_cost_per_sqft=3.75,
+        tiling_area=3.00,
+        total_supply_cost=0
+    )
+
+    test_session.add(new_room)
+    test_session.commit()
+
+    new_room.total_supply_cost()
