e25c114394ddf93f4896153a4651da8ab241339e
Isaiah <Isaiahaperez12@gmail.com>
2024-11-04 19:20:10 -0600

    Testing the seaborn

--- main.py
+++ main.py
@@ -1,14 +1,88 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
-from sqlalchemy.orm import sessionmaker, declarative_base, relationship
+from flask import Flask, render_template, request, redirect, url_for, flash
 from models.model import *
 import seaborn as sns
 import matplotlib.pyplot as plt
 import os
 
 app = Flask(__name__)
-app.secret_key = "RunCurrentFile"
+app.secret_key = "stdyfugihjfhgfyu"
+session = SS()
+@app.route("/")
+def index():
+    room_names = [room.name for room in session.query(Room).all()]
+    remodel_costs = [room.total_remodel_cost for room in session.query(Room).all()]
+    plt.figure(figsize=(10, 6))
+    sns.barplot(x=room_names, y=remodel_costs, palette="viridis")
+    plt.title("Total Remodeling Cost per Room")
+    plt.xlabel("Room")
+    plt.ylabel("Total Remodel Cost ($)")
+
+    img_path = os.path.join(app.root_path, "static", "plot.png")
+    plt.savefig(img_path)
+    plt.close()
+    return render_template('index.html', plot_url= url_for('static', filename='plot.png'))
+
+def get_all_rooms_being_Remodeled():
+    return session.query(Room).filter(Room.is_tiling_needed == 'y').all()
+
+@app.route("/supply_details", methods=['GET', 'POST'])
+def supply_details():
+    if request.method == 'POST':
+        supply_name = request.form['supply_name']
+        All_supplies = Get_all_supplies()
+
+        for supply in All_supplies:
+            if supply.name == supply_name:
+                Supply_details = Get_supply_by_name(supply_name)
+                return redirect(url_for('supply_details', Supply_details=Supply_details))
+            else:
+                flash("This Supply does not exist")
+                return redirect(url_for('supply_details'))
+    return render_template("supply_details.html")
+
+
+def Get_supply_by_name(supply_name):
+    return session.query(Supply).filter(Supply.name == supply_name).all()
+
+def Get_all_supplies():
+    return session.query(Supply).all()
+
+@app.route("/add_supply", methods=['GET', 'POST'])
+def add_supply():
+    if request.method == 'POST':
+        name = request.form['name']
+        quantity = int(request.form['quantity'])
+        cost = float(request.form['cost'])
+
+        if name == "":
+            flash("Please enter your name")
+            return redirect(url_for('add_supply'))
+        elif quantity <= 0:
+            flash("Please enter your quantity")
+            return redirect(url_for('add_supply'))
+        elif cost <= 0:
+            flash("Please enter your cost")
+            return redirect(url_for('add_supply'))
+        else:
+            supply=Supply(name, quantity, cost)
+            session.add(supply)
+            session.commit()
+            session.refresh(supply)
+            session.close()
+
+            flash("Your supply has been added")
+            return redirect(url_for('add_supply'))
+    return render_template('add_supply.html')
+
+@app.route('/add_room', methods=['GET', 'POST'])
+def add_room():
+    if request.method == 'POST':
+        name = request.form['name']
+        surface_area = request.form['surface_area']
+        flooring_type = request.form['flooring_type']
+
+    return render_template("add_room.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
